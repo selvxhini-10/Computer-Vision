69552003363e4878ea56c9789e57238327b9c066
selvxhini-10 <selvahinik10@gmail.com>
2025-03-11 21:52:23 -0400

    Added an Expander Box
    Added a survey option on the Projects Page

--- pages/1_📈_Projects.py
+++ pages/1_📈_Projects.py
@@ -28,7 +28,7 @@ custom_html = """
 st.components.v1.html(custom_html)
 
 st.sidebar.header("Applications of Image Segmentation")
-st.sidebar.text("Image Segmentation is used in a variety of industries, including healthcare, retail, automotive, agriculture and space.")
+st.sidebar.text("Image Segmentation is used in a variety of industries, including healthcare, retail, automotive, agriculture and astronomy.")
 
 st.title("Computer Vision Projects")
 
@@ -88,15 +88,18 @@ assert model_config_string.find('"groups": 1,') == -1
 # Load the pre-trained model
 model = load_model(r"C:/Users/selva/OneDrive/Desktop/openproject_CV_workshop/model/keras_model.h5", compile=False)
 
-class_names = open("C:/Users/selva/OneDrive/Desktop/openproject_CV_workshop/model/labels.txt", "r").readlines()
+class_names = open(r"C:/Users/selva/OneDrive/Desktop/openproject_CV_workshop/model/labels.txt", "r").readlines()
 
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Upload image
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-image = Image.open(uploaded_file).convert("RGB")
-st.image(image, caption="Uploaded Image", use_container_width=True)
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("RGB")
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+else:
+    st.warning("Please upload an image first.")
 
 # resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
@@ -117,8 +120,25 @@ index = np.argmax(prediction)
 class_name = class_names[index].strip()  # Remove whitespace
 confidence_score = round(prediction[0][index] * 100, 2)  # Convert to percentage
 
-    # Print prediction and confidence score
-st.subheader(f"Prediction: {class_name}")
-st.subheader(f"Confidence Score: {confidence_score}%")
+# Print prediction and confidence score
+st.subheader(f"Results")
+st.write(f"Prediction: {class_name}")
+st.write(f"Confidence Score: {confidence_score}%")
 
+# Results Survey
+import streamlit as st
 
+with st.expander("Was the AI model's prediction correct?"):
+    survey = st.radio(
+        "Select an option below.",
+        ["***Yes :white_check_mark:***", "***No :x:***"],
+        captions=[
+            "All hail AI.",
+            "Nah, it sucks."
+        ],
+    )
+
+    if survey == "***Yes :white_check_mark:***":
+        st.write("Your response is noted :)")
+    else:
+        st.write("We will work on improving the accuracy of the model.")
